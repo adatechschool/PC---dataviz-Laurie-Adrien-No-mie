b7b5b651aff8d1df4cd186d4dbbc31fae69546eb
laurie cluzeau <lauriecluzeau@MacBook-Pro-de-laurie.local>
2021-08-03 16:14:22 +0200

    affichage de toutes nos data

--- src/prenom.js
+++ src/prenom.js
@@ -1,23 +1,20 @@
 var listPrenoms = [];
+var prenoms = [];
+var nombre = [];
 
   fetch('https://opendata.paris.fr/api/records/1.0/search/?dataset=liste_des_prenoms&q=&rows=20&sort=annee&facet=annee&facet=prenoms&facet=nombre&facet=nombre_total_cumule') 
         .then(function(response){
              listPrenoms = response.json();
-             console.log(listPrenoms);
              return listPrenoms;  
         })
         .then(listPrenoms =>{
-          for(let i=0; i<listPrenoms.records.length; i++){
-            //console.log(listPrenoms.records[i].fields.prenoms);
-            let prenoms = listPrenoms.records[i].fields.prenoms;
-            document.write(prenoms + "<br>");
-          }
-          for(let i=0; i<listPrenoms.records.length; i++){
-            //console.log(listPrenoms.records[i].fields.nombre);
-            let nombre = listPrenoms.records[i].fields.nombre;
-            document.write(nombre + "<br>");
-          }
-        })
+            for(let i=0; i<listPrenoms.records.length; i++){
+              prenoms[i]= listPrenoms.records[i].fields.prenoms;
+             document.getElementById("prenoms").innerHTML = prenoms;
+              nombre[i] =  listPrenoms.records[i].fields.nombre;
+             document.getElementById("nombre").innerHTML = nombre;
+            }
+          })
         .catch((error) => {
             console.log('rejected',error);
         });
