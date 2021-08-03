6f327e4a9a0e0c5d3f25cfcd84b08f37dffb51fd
Noémie Ragot <noemieprog@gmail.com>
2021-08-03 12:05:35 +0200

    boucle for unique

--- src/prenom.js
+++ src/prenom.js
@@ -1,27 +1,24 @@
 var listPrenoms = [];
 
+
   fetch('https://opendata.paris.fr/api/records/1.0/search/?dataset=liste_des_prenoms&q=&rows=20&sort=annee&facet=annee&facet=prenoms&facet=nombre&facet=nombre_total_cumule') 
         .then(function(response){
              listPrenoms = response.json();
-             console.log(listPrenoms);
+             //console.log(listPrenoms);
              return listPrenoms;  
         })
         .then(listPrenoms =>{
           for(let i=0; i<listPrenoms.records.length; i++){
             //console.log(listPrenoms.records[i].fields.prenoms);
-            let prenoms = listPrenoms.records[i].fields.prenoms;
+            prenoms = [listPrenoms.records[i].fields.prenoms];
+            prenoms.toString();            
             document.write(prenoms + "<br>");
-          }
-          for(let i=0; i<listPrenoms.records.length; i++){
-            //console.log(listPrenoms.records[i].fields.nombre);
-            let nombre = listPrenoms.records[i].fields.nombre;
+            let nombre = [listPrenoms.records[i].fields.nombre];
             document.write(nombre + "<br>");
+            console.log(prenoms, nombre)
           }
         })
         .catch((error) => {
             console.log('rejected',error);
         });
 
-
-
-        
